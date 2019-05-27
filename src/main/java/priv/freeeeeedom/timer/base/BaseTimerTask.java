225d7fcaf1bfda5ee94107619ab3867b66360f78
Nevernow <wjj8251107@hotmail.com>
2019-05-27 19:28:15 +0800

    更新代码逻辑

---
+++ src/main/java/priv/freeeeeedom/timer/base/BaseTimerTask.java
@@ -0,0 +1,44 @@
+package priv.freeeeeedom.timer.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import priv.freeeeeedom.utils.ResultUtils;
+
+/**
+ * 定时任务抽象类
+ *
+ * @author: Nevernow
+ * @Date: 17:09 2019/5/21
+ */
+public abstract class BaseTimerTask<T> implements BaseTimer {
+    /**
+     * log对象
+     */
+    private static Logger log = LoggerFactory.getLogger(BaseTimerTask.class);
+
+    /**
+     * 执行任务核心逻辑
+     *
+     * @param: []
+     * @return: T
+     * @author: Nevernow
+     * @Date: 2019/5/21 17:11
+     */
+    protected abstract T runTask() throws Exception;
+
+    @Override
+    public Object startTimer() {
+        try {
+            return this.runTask();
+        } catch (Exception e) {
+            log.error("定时任务执行异常!", e);
+            return ResultUtils.getExceptionLineSize(e, 7);
+        }
+    }
+
+    @Override
+    public Object startTimer(Object... param) {
+        return null;
+    }
+
+}
